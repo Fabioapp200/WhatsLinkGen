f7f967de505a635acf735426d733e478bce1b6ed
Fábio Pinto <pintofabio100@gmail.com>
2022-05-12 15:24:00 -0300

    Live version, changed branding to WhatsLink Generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'theme_model.dart';
 
 void main() {
-  runApp(const WhatsappLinkGen());
-}
-
-class WhatsappLinkGen extends StatelessWidget {
-  const WhatsappLinkGen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MainInterface(),
-    );
-  }
+  runApp(const WhatsLinkGen());
 }
 
 var currPhoneNumber = "";
 
-class MainInterface extends StatelessWidget {
-  const MainInterface({Key? key}) : super(key: key);
+class WhatsLinkGen extends StatelessWidget {
+  const WhatsLinkGen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Whatsapp Link Generator'),
-        // actions: <Widget>[
-        //   IconButton(
-        //     icon: Icon(Icons.more_vert),
-        //     onPressed: () {},
-        //   )
-        // ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                IntlPhoneField(
-                  initialCountryCode: "BR",
-                  onChanged: (phone) {
-                    currPhoneNumber = phone.completeNumber;
-                  },
-                  disableLengthCheck: true,
-                  decoration: InputDecoration(
-                      labelText: "Phone Number", hintText: "12 3 4567-8910"),
+    return ChangeNotifierProvider(
+      create: (_) => ThemeModel(),
+      child: Consumer<ThemeModel>(
+          builder: (context, ThemeModel themeNotifier, child) {
+        return MaterialApp(
+          title: 'WhatsLink Generator',
+          theme: themeNotifier.isDark
+              ? ThemeData(
+                  brightness: Brightness.dark,
+                  primarySwatch: Colors.green,
+                )
+              : ThemeData(
+                  brightness: Brightness.light,
+                  primarySwatch: Colors.green,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _launchURL();
-                    },
-                    child: Text('Open chat on Whatsapp'),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+          debugShowCheckedModeBanner: false,
+          home: const MainInterface(),
+        );
+      }),
     );
   }
 }
@@ -75,4 +42,66 @@ void _launchURL() async {
   var _url =
       "https://api.whatsapp.com/send?phone=" + currPhoneNumber.substring(1);
   if (!await launch(_url)) throw 'Could not launch $_url';
+}
+
+class MainInterface extends StatefulWidget {
+  const MainInterface({Key? key}) : super(key: key);
+
+  @override
+  _MainInterfaceState createState() => _MainInterfaceState();
+}
+
+class _MainInterfaceState extends State<MainInterface> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeModel>(
+        builder: (context, ThemeModel themeNotifier, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsLink Generator'),
+          actions: [
+            IconButton(
+                icon: Icon(
+                    themeNotifier.isDark ? Icons.dark_mode : Icons.light_mode),
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                })
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  IntlPhoneField(
+                    initialCountryCode: "BR",
+                    onChanged: (phone) {
+                      currPhoneNumber = phone.completeNumber;
+                    },
+                    disableLengthCheck: true,
+                    decoration: const InputDecoration(
+                      labelText: "Phone Number",
+                      hintText: "12 3 4567-8910",
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _launchURL();
+                      },
+                      child: const Text('Start Chatting!'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
 }
